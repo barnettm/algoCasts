f76b23def50edb977b73c1059c8b3aa7cc9a1322
barnettm <barnett717@gmail.com>
2018-03-22 12:39:47 -0700

    string reversal two

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// take string and turn into an array. 
+// loop array in reverse and add to new array
+// turn new array into one string
+// return new string
+
+
+// function reverse(str) {
+//      return str.split('').reverse().join('')
+// }
+
+function reverse(str) {
+    let reverse = '';
+    for(let character of str) {
+        reverse = character + reverse
+    }
+    return reverse;
+}
+
+
+// const reverse = (str) => str.split('').reverse().join('')
+
+
+// console.log(reverse('apple'))
+
+
 
 module.exports = reverse;
