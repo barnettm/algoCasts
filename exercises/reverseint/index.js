13d7f538476e61f274af9f0c7968b3fb4e3ba963
barnettm <barnett717@gmail.com>
2018-03-23 12:07:16 -0700

    reverseint two

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,16 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+// function reverseInt(n) {
+//     let newArray = n.toString().split('').reverse()
+//     if(newArray[newArray.length -1] === '-') {
+//         let lastElement = newArray.pop()
+//         newArray.unshift(lastElement)
+//     }
+//     newArray = newArray.join('')
+//     newArray = parseFloat(newArray)
+//     return newArray
+// }
+
 function reverseInt(n) {
-    let newArray = n.toString().split('').reverse()
-    if(newArray[newArray.length -1] === '-') {
-        let lastElement = newArray.pop()
-        newArray.unshift(lastElement)
-    }
-    newArray = newArray.join('')
-    newArray = parseFloat(newArray)
-    return newArray
+    const reverse = n.toString().split('').reverse().join('');
+    return parseInt(reverse) * Math.sign(n)
 }
+
+
+
 
 // console.log(reverseInt(-15))
 
