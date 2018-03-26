dae530b627622ecd78a039846df23c59ec2f7bd4
barnettm <barnett717@gmail.com>
2018-03-26 15:30:48 -0700

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,14 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {};
+    for(let char of str) {
+        chars[char] = chars[char] + 1 || 1;    
+    }
+    console.log(chars)
+}
 
-module.exports = maxChar;
+// module.exports = maxChar;
+
+maxChar("Hello There")
