a2997b8ceaefef25f31103b1d75d4d8114a4fbb4
barnettm <barnett717@gmail.com>
2018-03-26 15:37:36 -0700

    maxChar sol 1

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,13 +6,29 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-    const chars = {};
-    for(let char of str) {
-        chars[char] = chars[char] + 1 || 1;    
+    const charMap = {}
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        if(charMap[char]) {
+            charMap[char]++
+        }else{
+            charMap[char] = 1
+        }
     }
-    console.log(chars)
+    for(char in charMap) {
+        if(charMap[char] > max){
+            max = charMap[char]
+            maxChar = char
+        }
+    }
+    return maxChar
 }
 
-// module.exports = maxChar;
 
-maxChar("Hello There")
+
+
+
+module.exports = maxChar;
+
